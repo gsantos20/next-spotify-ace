242664ebaac346f53f673a4d5c7a5003d099cce9
Gustavo Neris <nerisgs20@gmail.com>
2023-07-06 01:04:07 -0300

    feat : layout and style index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,21 @@ import React from 'react'
 import Head from 'next/head'
 import Header from '../components/header'
 import Footer from '../components/footer'
+import { GlobalIndexStyle } from '../styles/pages/index'
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <>
+      <GlobalIndexStyle />
       <Head>
         <title>Ace</title>
+        <link rel="shortcut icon" href="images/ace.png" type="image/x-icon" />
       </Head>
 
       <Header></Header>
 
       <Footer></Footer>
-    </div>
+    </>
   )
 }
 
---
+++ src/styles/pages/index.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { createGlobalStyle } from 'styled-components'
+
+export interface IndexProps {}
+
+export const GlobalIndexStyle = createGlobalStyle`
+  body {
+    background-color: black
+  }
+`
