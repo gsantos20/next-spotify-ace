60bb9af9d35850e2a3f74d07fc409904542aecf3
Gustavo Neris <nerisgs20@gmail.com>
2023-08-14 23:03:33 -0300

    feat: responsive with 768

--- src/styles/components/header.ts
+++ src/styles/components/header.ts
@@ -40,7 +40,7 @@ export const Nav = styled.nav`
   width: 100%;
   height: 100%;
 
-  @media screen and (max-width: 768px) {
+  @media screen and (max-width: 767px) {
     position: fixed;
     top: 0;
     left: -100%;
